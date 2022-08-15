8bf5db107e786dffd34d05cef1ac8af6897eda4e
Nattaallia <kulbacka.natalya@gmail.com>
2022-08-16 01:04:40 +0200

    Add foreground work

--- app/src/main/java/com/raywenderlich/android/workmanager/workers/ImageDownloadWorker.kt
+++ app/src/main/java/com/raywenderlich/android/workmanager/workers/ImageDownloadWorker.kt
@@ -36,10 +36,14 @@
 
 package com.raywenderlich.android.workmanager.workers
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
-import androidx.work.CoroutineWorker
-import androidx.work.WorkerParameters
-import androidx.work.workDataOf
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.work.*
 import kotlinx.coroutines.delay
 import com.raywenderlich.android.workmanager.utils.getUriFromUrl
 
@@ -48,11 +52,49 @@ class ImageDownloadWorker(
     private val workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
     override suspend fun doWork(): Result {
+        setForeground(createForegroundInfo())
         // 1
         delay(10000)
         // 2
         val savedUri = context.getUriFromUrl()
         // 3
         return Result.success(workDataOf("IMAGE_URI" to savedUri.toString()))
+    }
+
+    private fun createForegroundInfo(): ForegroundInfo {
+        // 1
+        val intent = WorkManager.getInstance(applicationContext)
+            .createCancelPendingIntent(id)
+        // 2
+        val notification = NotificationCompat.Builder(
+            applicationContext, "workDownload")
+            .setContentTitle("Downloading Your Image")
+            .setTicker("Downloading Your Image")
+            .setSmallIcon(R.drawable.notification_action_background)
+            .setOngoing(true)
+            .addAction(android.R.drawable.ic_delete, "Cancel Download", intent)
+        // 3
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel(notification, "workDownload")
+        }
+        return ForegroundInfo(1, notification.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel(
+        notificationBuilder: NotificationCompat.Builder,
+        id: String
+    ) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as
+                    NotificationManager
+        notificationBuilder.setDefaults(Notification.DEFAULT_VIBRATE)
+        val channel = NotificationChannel(
+            id,
+            "WorkManagerApp",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel.description = "WorkManagerApp Notifications"
+        notificationManager.createNotificationChannel(channel)
     }
 }
